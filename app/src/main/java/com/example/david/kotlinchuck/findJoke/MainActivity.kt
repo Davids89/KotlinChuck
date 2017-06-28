54f4054fc7daff24b3d594a737018be5ebd59e91
Davids89 <daviluqui@gmail.com>
2017-06-28 20:09:46 +0200

    MainActivity in findJoke package

--- app/src/main/java/com/example/david/kotlinchuck/findJoke/MainActivity.kt
+++ app/src/main/java/com/example/david/kotlinchuck/findJoke/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.david.kotlinchuck
+package com.example.david.kotlinchuck.findJoke
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.david.kotlinchuck.R
 
 class MainActivity : AppCompatActivity() {
 
