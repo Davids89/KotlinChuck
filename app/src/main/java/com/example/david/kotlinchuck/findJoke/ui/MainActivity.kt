149bec1bbd8c36ae3d63e4d3933797c4d00b02aa
Davids89 <daviluqui@gmail.com>
2017-06-29 21:29:46 +0200

    Presenter declaration in activity

--- app/src/main/java/com/example/david/kotlinchuck/findJoke/ui/MainActivity.kt
+++ app/src/main/java/com/example/david/kotlinchuck/findJoke/ui/MainActivity.kt
@@ -4,12 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.david.kotlinchuck.R
 import com.example.david.kotlinchuck.entities.Joke
+import com.example.david.kotlinchuck.findJoke.FindJokePresenter
+import com.example.david.kotlinchuck.findJoke.FindJokePresenterImpl
 
 class MainActivity : AppCompatActivity(), FindJokeView {
+
+    lateinit var presenter : FindJokePresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        presenter = FindJokePresenterImpl()
+        presenter.onCreate()
     }
 
     override fun jokeSuccess(joke: Joke) {
@@ -18,5 +25,10 @@ class MainActivity : AppCompatActivity(), FindJokeView {
 
     override fun jokeError() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
     }
 }
