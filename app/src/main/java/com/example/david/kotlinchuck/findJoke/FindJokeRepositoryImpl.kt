f90c3b12c5c6e44f39d9fb262210db717b249d4d
Davids89 <daviluqui@gmail.com>
2017-06-30 20:13:34 +0200

    Emit the event when receive the response from the API

--- app/src/main/java/com/example/david/kotlinchuck/findJoke/FindJokeRepositoryImpl.kt
+++ app/src/main/java/com/example/david/kotlinchuck/findJoke/FindJokeRepositoryImpl.kt
@@ -2,6 +2,8 @@ package com.example.david.kotlinchuck.findJoke
 
 import com.example.david.kotlinchuck.api.ChuckClient
 import com.example.david.kotlinchuck.api.RandomJokeResponse
+import com.example.david.kotlinchuck.entities.Joke
+import com.example.david.kotlinchuck.findJoke.event.FindJokeEvent
 import com.example.david.kotlinchuck.lib.EventBus
 import com.example.david.kotlinchuck.lib.GreenRobotEventBus
 import retrofit2.Call
@@ -14,23 +16,46 @@ import retrofit2.Response
 class FindJokeRepositoryImpl: FindJokeRepository {
 
     private val client: ChuckClient = ChuckClient()
-    private val eventBus: EventBus = GreenRobotEventBus.INSTANCE
+    val eventBus: EventBus = GreenRobotEventBus.INSTANCE
 
     override fun findJoke(name: String, lastName: String) {
 
         client.callToChuck().getRandomWithName(name, lastName).enqueue()
     }
 
-}
+    private fun success(joke: Joke){
+        post(joke, false)
+    }
 
-private fun <T> Call<T>.enqueue() {
-    enqueue(object: Callback<T>{
-        override fun onResponse(call: Call<T>?, response: Response<T>?) {
-            println(response?.body().toString())
+    private fun error(){
+        post(null, true)
+    }
+
+    private fun post(joke: Joke?, error: Boolean){
+
+        var event: FindJokeEvent = FindJokeEvent()
+
+        if(!error){
+            event.type = FindJokeEvent.onSuccess
+            event.joke = joke
+        } else {
+            event.type = FindJokeEvent.onError
         }
 
-        override fun onFailure(call: Call<T>?, t: Throwable?) {
-            println(t?.localizedMessage.toString())
-        }
-    })
+        eventBus.post(event)
+    }
+
+    private fun Call<RandomJokeResponse>.enqueue() {
+        enqueue(object: Callback<RandomJokeResponse>{
+            override fun onResponse(call: Call<RandomJokeResponse>?, response: Response<RandomJokeResponse>?) {
+                success(response?.body()!!.value)
+            }
+
+            override fun onFailure(call: Call<RandomJokeResponse>?, t: Throwable?) {
+                println(t?.localizedMessage.toString())
+                error()
+            }
+        })
+    }
+
 }
