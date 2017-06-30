0902048548e457b9aac9e8fddcfbe65f3da08c39
Davids89 <daviluqui@gmail.com>
2017-06-30 19:37:38 +0200

    Setting the view

--- app/src/main/java/com/example/david/kotlinchuck/findJoke/ui/MainActivity.kt
+++ app/src/main/java/com/example/david/kotlinchuck/findJoke/ui/MainActivity.kt
@@ -2,21 +2,38 @@ package com.example.david.kotlinchuck.findJoke.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.david.kotlinchuck.R
 import com.example.david.kotlinchuck.entities.Joke
 import com.example.david.kotlinchuck.findJoke.FindJokePresenter
 import com.example.david.kotlinchuck.findJoke.FindJokePresenterImpl
+import org.jetbrains.anko.find
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity(), FindJokeView {
 
     lateinit var presenter : FindJokePresenter
+    var name: TextView? = null
+    var lastName: TextView? = null
+    var button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setViews()
+
         presenter = FindJokePresenterImpl(this)
         presenter.onCreate()
+    }
+
+    private fun setViews() {
+        button = find<Button>(R.id.findJoke)
+        name = find<TextView>(R.id.name)
+        lastName = find<TextView>(R.id.lastName)
+
+        button?.onClick { searchJoke(name?.text.toString(), lastName?.text.toString()) }
     }
 
     override fun jokeSuccess(joke: Joke) {
@@ -30,5 +47,9 @@ class MainActivity : AppCompatActivity(), FindJokeView {
     override fun onDestroy() {
         presenter.onDestroy()
         super.onDestroy()
+    }
+
+    private fun searchJoke(name: String, lastName: String){
+        presenter.findJoke(name, lastName)
     }
 }
