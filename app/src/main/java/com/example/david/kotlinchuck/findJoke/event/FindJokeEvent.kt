0ae2fe0d71259f473f72b0366ec745d8e33887fa
Davids89 <daviluqui@gmail.com>
2017-06-30 20:12:56 +0200

    Fixing event

--- app/src/main/java/com/example/david/kotlinchuck/findJoke/event/FindJokeEvent.kt
+++ app/src/main/java/com/example/david/kotlinchuck/findJoke/event/FindJokeEvent.kt
@@ -6,10 +6,13 @@ import com.example.david.kotlinchuck.entities.Joke
  * Created by david on 28/6/17.
  */
 
-class FindJokeEvent(){
-    val onSuccess = 0
-    val onError = 1
+class FindJokeEvent{
 
-    private var type: Int? = null
-    private var joke: Joke? = null
+    companion object {
+        val onSuccess = 0
+        val onError = 1
+    }
+
+    var type: Int? = null
+    var joke: Joke? = null
 }
