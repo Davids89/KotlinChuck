794872a2ccd8abe8d14f7acd340357d46909058c
Davids89 <daviluqui@gmail.com>
2017-07-08 13:59:52 +0200

    Setting the fragment logic in the activity

--- app/src/main/java/com/example/david/kotlinchuck/mainActivity/ui/MainActivity.kt
+++ app/src/main/java/com/example/david/kotlinchuck/mainActivity/ui/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.david.kotlinchuck.mainActivity.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import com.example.david.kotlinchuck.R
+import com.example.david.kotlinchuck.mainActivity.ui.fragments.FavoriteFragment
+import com.example.david.kotlinchuck.mainActivity.ui.fragments.SearchFragment
+import com.example.david.kotlinchuck.mainActivity.ui.fragments.SettingsFragment
 import org.jetbrains.anko.find
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -11,25 +17,41 @@ import org.jetbrains.anko.info
 class MainActivity : AppCompatActivity() {
 
     lateinit var bottomNavigation: BottomNavigationView
+    val fragmentManager: FragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setInitialFragment()
+        setupFragments()
         setBottomNavigationBar()
+    }
+
+    private fun setInitialFragment() {
+        val searchFragment: SearchFragment = SearchFragment()
+        val transaction : FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.add(R.id.main_container, searchFragment).commit()
+    }
+
+    private fun setupFragments() {
+
     }
 
     private fun setBottomNavigationBar() {
         bottomNavigation = find(R.id.bottom_navigation)
+        var fragment: Fragment = Fragment()
 
         bottomNavigation.setOnNavigationItemSelectedListener{
 
-            val intent = when(it.itemId){
-                R.id.action_search -> println("Search")
-                R.id.action_favorite -> println("Favorite")
-                R.id.action_settings -> println("Settings")
-                else -> null
+            when(it.itemId){
+                R.id.action_search -> fragment = SearchFragment()
+                R.id.action_favorite -> fragment = FavoriteFragment()
+                R.id.action_settings -> fragment = SettingsFragment()
             }
+
+            val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.main_container, fragment).commit()
 
             true
         }
