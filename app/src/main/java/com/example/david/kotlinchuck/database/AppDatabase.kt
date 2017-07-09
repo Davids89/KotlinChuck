1f4d4f0cb9c1ea0066eafcb80086ea3fec1cfd42
Davids89 <daviluqui@gmail.com>
2017-07-09 11:36:42 +0200

    AppDatabase configuration

---
+++ app/src/main/java/com/example/david/kotlinchuck/database/AppDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.david.kotlinchuck.database
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import com.example.david.kotlinchuck.entities.Joke
+
+/**
+ * Created by david on 9/7/17.
+ */
+
+@Database(entities = arrayOf(Joke::class), version = 1)
+abstract class AppDatabase: RoomDatabase(){
+    abstract fun jokeDao(): JokeDAO
+}
