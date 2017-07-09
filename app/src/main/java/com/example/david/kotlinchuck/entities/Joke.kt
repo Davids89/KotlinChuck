1af286de2d08cbf6a69c2c9c741c80faa3d74581
Davids89 <daviluqui@gmail.com>
2017-07-09 11:37:30 +0200

    Editting the entity to room

--- app/src/main/java/com/example/david/kotlinchuck/entities/Joke.kt
+++ app/src/main/java/com/example/david/kotlinchuck/entities/Joke.kt
@@ -1,11 +1,16 @@
 package com.example.david.kotlinchuck.entities
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by david on 28/6/17.
  */
 
+@Entity
 data class Joke(
+        @PrimaryKey(autoGenerate = true)
+        val uid: Long,
         @SerializedName("id") val id: String,
         @SerializedName("joke") val joke: String)
