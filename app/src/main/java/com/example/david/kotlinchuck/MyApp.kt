fd501953b787dfd796db5c7b065aedf7cd7b4b28
Davids89 <daviluqui@gmail.com>
2017-08-08 11:34:57 +0200

    Context application to get the resources out of the activity

--- app/src/main/java/com/example/david/kotlinchuck/MyApp.kt
+++ app/src/main/java/com/example/david/kotlinchuck/MyApp.kt
@@ -2,6 +2,7 @@ package com.example.david.kotlinchuck
 
 import android.app.Application
 import android.arch.persistence.room.Room
+import android.content.Context
 import com.example.david.kotlinchuck.database.AppDatabase
 
 /**
@@ -10,10 +11,14 @@ import com.example.david.kotlinchuck.database.AppDatabase
 class MyApp: Application() {
     companion object {
         var database: AppDatabase? = null
+        var context: Context? = null
     }
+
+
 
     override fun onCreate() {
         super.onCreate()
         MyApp.database = Room.databaseBuilder(this, AppDatabase::class.java, "chuck-joke-db").build()
+        context = this
     }
 }
