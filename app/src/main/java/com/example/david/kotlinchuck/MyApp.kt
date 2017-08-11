73836bbceec0e084e89203a9b7635cfb3a9df39b
Davids89 <daviluqui@gmail.com>
2017-08-11 12:18:51 +0200

    Injection in app scope

--- app/src/main/java/com/example/david/kotlinchuck/MyApp.kt
+++ app/src/main/java/com/example/david/kotlinchuck/MyApp.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import android.arch.persistence.room.Room
 import android.content.Context
 import com.example.david.kotlinchuck.database.AppDatabase
+import com.example.david.kotlinchuck.di.AppComponent
+import com.example.david.kotlinchuck.di.AppModule
+import com.example.david.kotlinchuck.di.DaggerAppComponent
 
 /**
  * Created by david on 9/7/17.
@@ -11,14 +14,17 @@ import com.example.david.kotlinchuck.database.AppDatabase
 class MyApp: Application() {
     companion object {
         var database: AppDatabase? = null
-        var context: Context? = null
+        lateinit var context: Context
+        lateinit var component: AppComponent
     }
-
-
 
     override fun onCreate() {
         super.onCreate()
         MyApp.database = Room.databaseBuilder(this, AppDatabase::class.java, "chuck-joke-db").build()
         context = this
+
+        component = DaggerAppComponent.builder()
+                .appModule(AppModule(context))
+                .build()
     }
 }
