160d42bba5d24798436f53bf31549baf50f876ad
Davids89 <daviluqui@gmail.com>
2017-08-12 19:23:40 +0200

    Favorite presenter with the basic implementation

--- app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/presenter/FavoriteJokesPresenterImpl.kt
+++ app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/presenter/FavoriteJokesPresenterImpl.kt
@@ -1,7 +1,42 @@
 package com.example.david.kotlinchuck.favoriteJokes.presenter
 
+import com.example.david.kotlinchuck.MyApp
+import com.example.david.kotlinchuck.favoriteJokes.event.FavoriteEvent
+import com.example.david.kotlinchuck.favoriteJokes.repository.FavoriteJokesRepository
+import com.example.david.kotlinchuck.favoriteJokes.ui.FavoriteView
+import com.example.david.kotlinchuck.lib.base.EventBus
+import org.greenrobot.eventbus.Subscribe
+import javax.inject.Inject
+
 /**
  * Created by david on 10/7/17.
  */
-class FavoriteJokesPresenterImpl: FavoriteJokesPresenter {
+class FavoriteJokesPresenterImpl(var view: FavoriteView?): FavoriteJokesPresenter {
+
+    @Inject
+    lateinit var eventBus: EventBus
+
+    @Inject
+    lateinit var repository: FavoriteJokesRepository
+
+    override fun onCreate() {
+
+        MyApp.presenterComponent().inject(this)
+
+        if(view != null){
+            eventBus.register(this)
+            repository.getJokes()
+        }
+    }
+
+    override fun onDestroy() {
+        eventBus.unregister(this)
+        view = null
+    }
+
+    @Subscribe
+    override fun onEventMainThread(event: FavoriteEvent) {
+
+    }
+
 }
