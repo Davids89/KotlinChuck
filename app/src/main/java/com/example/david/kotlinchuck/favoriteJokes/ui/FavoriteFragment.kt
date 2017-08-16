e45c2c51e821fdc5a68509b14ffc428db8dcf815
Davids89 <daviluqui@gmail.com>
2017-08-16 11:52:32 +0200

    Implementation of favoriteview and the recyclerview configuration

--- app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/ui/FavoriteFragment.kt
@@ -3,13 +3,21 @@ package com.example.david.kotlinchuck.favoriteJokes.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.david.kotlinchuck.MyApp
 
 import com.example.david.kotlinchuck.R
+import com.example.david.kotlinchuck.entities.Joke
 import com.example.david.kotlinchuck.favoriteJokes.presenter.FavoriteJokesPresenter
+import com.example.david.kotlinchuck.favoriteJokes.ui.adapter.JokesAdapter
+import kotlinx.android.synthetic.main.fragment_favorite.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.find
+import org.jetbrains.anko.support.v4.find
 import javax.inject.Inject
 
 
@@ -20,23 +28,50 @@ class FavoriteFragment : Fragment(), FavoriteView {
 
     @Inject
     lateinit var presenter: FavoriteJokesPresenter
+    lateinit var adapter: JokesAdapter
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         MyApp.favoriteFragmentComponent(this).inject(this)
-
-        presenter.onCreate()
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_favorite, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_favorite, container, false)
+
+        setupAdapter()
+        setupRecyclerView(view)
+        presenter.onCreate()
+
+        return view
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     override fun onDestroy() {
         super.onDestroy()
         presenter.onDestroy()
+    }
+
+    override fun onListJokesSuccess(jokeList: List<Joke>?) {
+        adapter.setJokes(jokeList)
+    }
+
+    override fun onListJokesError() {
+        snackbar(favoriteContext, "Error")
+    }
+
+    private fun setupRecyclerView(view: View?) {
+        recyclerView = view!!.find(R.id.savedJokesRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupAdapter() {
+        adapter = JokesAdapter()
     }
 }
