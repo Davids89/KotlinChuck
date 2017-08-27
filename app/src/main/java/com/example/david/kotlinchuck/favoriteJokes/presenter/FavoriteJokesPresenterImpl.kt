7568428c7219fc818997fdc344019546d91ce353
Davids89 <daviluqui@gmail.com>
2017-08-27 13:18:44 +0200

    Method to delete the joke in the presenter and to call the repository

--- app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/presenter/FavoriteJokesPresenterImpl.kt
+++ app/src/main/java/com/example/david/kotlinchuck/favoriteJokes/presenter/FavoriteJokesPresenterImpl.kt
@@ -1,6 +1,7 @@
 package com.example.david.kotlinchuck.favoriteJokes.presenter
 
 import com.example.david.kotlinchuck.MyApp
+import com.example.david.kotlinchuck.favoriteJokes.event.DeleteEvent
 import com.example.david.kotlinchuck.favoriteJokes.event.FavoriteEvent
 import com.example.david.kotlinchuck.favoriteJokes.repository.FavoriteJokesRepository
 import com.example.david.kotlinchuck.favoriteJokes.ui.FavoriteView
@@ -12,6 +13,7 @@ import javax.inject.Inject
  * Created by david on 10/7/17.
  */
 class FavoriteJokesPresenterImpl(var view: FavoriteView?): FavoriteJokesPresenter {
+
 
     @Inject
     lateinit var eventBus: EventBus
@@ -34,11 +36,23 @@ class FavoriteJokesPresenterImpl(var view: FavoriteView?): FavoriteJokesPresente
         view = null
     }
 
+    override fun deleteJoke(text: String) {
+        repository.deleteJoke(text)
+    }
+
     @Subscribe
     override fun onEventMainThread(event: FavoriteEvent) {
         when(event.type){
             FavoriteEvent.onSuccess -> view?.onListJokesSuccess(event.jokes)
             FavoriteEvent.onError -> view?.onListJokesError()
+        }
+    }
+
+    @Subscribe
+    override fun onDeleteEvent(event: DeleteEvent) {
+        when(event.type){
+            DeleteEvent.onSuccess -> view?.onDeleteSuccess(event.jokes)
+            DeleteEvent.onError -> view?.onDeleteError()
         }
     }
 
